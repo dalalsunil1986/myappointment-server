9e61af765a67e60a2b2632cc02901b20710cc541
ZaL <z4ls@live.com>
2016-02-18 01:28:14 +0300

    add more seeders

--- database/seeds/AppointmentsTableSeeder.php
+++ database/seeds/AppointmentsTableSeeder.php
@@ -12,6 +12,18 @@ class AppointmentsTableSeeder extends Seeder
     public function run()
     {
         //
-        factory(App\Src\Appointment\Appointment::class,25)->create();
+        factory(App\Src\Appointment\Appointment::class,25)->make()->each(function($appointment){
+            $company = App\Src\Company\Company::orderByRaw("RAND()")->first();
+            $service = App\Src\Service\Service::orderByRaw("RAND()")->first();
+            $employee = App\Src\Employee\Employee::orderByRaw("RAND()")->first();
+            $timing = App\Src\Timing\Timing::orderByRaw("RAND()")->first();
+            $user = App\Src\User\User::orderByRaw("RAND()")->first();
+            $appointment->company_id = $company->id;
+            $appointment->service_id = $service->id;
+            $appointment->employee_id = $employee->id;
+            $appointment->timing_id = $timing->id;
+            $appointment->user_id = $user->id;
+            $appointment->save();
+        });
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         'company_categories',
         'holidays',
         'employees',
-        'user_companies',
+        'company_users',
         'timings'
     ];
     /**
@@ -35,7 +35,7 @@ class DatabaseSeeder extends Seeder
         $this->call(TimingsTableSeeder::class);
         $this->call(EmployeesTableSeeder::class);
         $this->call(HolidaysTableSeeder::class);
-//        $this->call(AppointmentsTableSeeder::class);
+        $this->call(AppointmentsTableSeeder::class);
         Model::reguard();
 
     }
