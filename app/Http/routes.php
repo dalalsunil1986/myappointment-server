ef06f2406371e95a296e5706ffc811d8d13ee921
ZaL <z4ls@live.com>
2016-03-03 13:13:18 +0300

    fix route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -41,7 +41,7 @@ Route::group(['prefix' => 'api/v1'], function () {
 //    Route::get('appointments','ProfileController@getAppointments');
 
     Route::post('appointments/make','ProfileController@makeAppointment');
-    Route::post('appointments/cancel','ProfileController@cancelAppointment');
+//    Route::post('appointments/cancel','ProfileController@cancelAppointment');
 
     //make favorite
     Route::get('companies/{id}/favorite','ProfileController@favoriteCompany');
